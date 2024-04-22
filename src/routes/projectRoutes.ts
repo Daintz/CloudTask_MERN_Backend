af2e451c262382630a53da51c38a8af469cbbfb1
Daintzz <dafeloru@gmail.com>
2024-04-22 17:35:45 -0500

    feat: :safety_vest: add validations
    add validations in module project

--- src/routes/projectRoutes.ts
+++ src/routes/projectRoutes.ts
@@ -1,9 +1,20 @@
 import { Router } from "express";
+import { body } from "express-validator"
 import { projectController } from "../controllers/projectController";
+import { handleInputErrors } from "../middleware/validation";
 
 const router = Router();
 
-router.post('/', projectController.createProject);
+router.post('/',
+  body('projectName')
+    .notEmpty().withMessage('The name of project is obligatory'),
+  body('clientName')
+    .notEmpty().withMessage('The client name of project is obligatory'),
+  body('description')
+    .notEmpty().withMessage('The description of project is obligatory'),
+  handleInputErrors,
+  projectController.createProject
+);
 router.get('/', projectController.getAllProjects);
 
 export default router;
